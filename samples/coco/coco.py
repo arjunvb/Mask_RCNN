270f34ae35481fb884cc37a892757f433989e99d
Nivi <nisrinivasan@ctemc.org>
2022-02-11 22:38:05 +0000

    filter class

--- samples/coco/coco.py
+++ samples/coco/coco.py
@@ -525,7 +525,7 @@ if __name__ == '__main__':
         # Validation dataset
         dataset_val = CocoDataset()
         val_type = "val" if args.year in '2017' else "minival"
-        coco = dataset_val.load_coco(args.dataset, val_type, year=args.year, return_coco=True, auto_download=args.download)
+        coco = dataset_val.load_coco(args.dataset, val_type, year=args.year, return_coco=True, auto_download=args.download, class_ids=[3])
         dataset_val.prepare()
         print("Running COCO evaluation on {} images.".format(args.limit))
         evaluate_coco(model, dataset_val, coco, "bbox", limit=int(args.limit))
