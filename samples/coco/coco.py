cce7f9752936ec64d46a735e737ac8c17b5b3f3b
Nivi <nisrinivasan@ctemc.org>
2022-02-12 00:41:49 +0000

    changed args in load_weights

--- samples/coco/coco.py
+++ samples/coco/coco.py
@@ -471,7 +471,7 @@ if __name__ == '__main__':
 
     # Load weights
     print("Loading weights ", model_path)
-    model.load_weights(model_path, by_name=True)
+    model.load_weightsmodel.load_weights(model_path, by_name=True, exclude=[ "mrcnn_class_logits", "mrcnn_bbox_fc", "mrcnn_bbox", "mrcnn_mask"])
 
     # Train or evaluate
     if args.command == "train":
